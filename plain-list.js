941f78d80cc6b886a2b1778ec2899a408ae01ed8
Alex Kirk <akirk@users.noreply.github.com>
2026-09-10 10:40:17 +0200

    Address WordPress.org inline asset review items

---
+++ plain-list.js
@@ -0,0 +1,66 @@
+(function () {
+	'use strict';
+
+	function setAll(checked) {
+		document.querySelectorAll('input[type="checkbox"]').forEach(function (checkbox) {
+			checkbox.checked = checked;
+		});
+	}
+
+	function reverseList() {
+		var items = Array.prototype.slice.call(document.querySelectorAll('li'));
+		if (!items.length) {
+			return;
+		}
+
+		var parent = items[0].parentNode;
+		items.forEach(function (item) {
+			parent.insertBefore(item, parent.firstChild);
+		});
+	}
+
+	function moveItem(link, direction) {
+		var item = link.closest('li');
+		if (!item) {
+			return;
+		}
+
+		var sibling = direction === 'up' ? item.previousElementSibling : item.nextElementSibling;
+		if (!sibling) {
+			return;
+		}
+
+		if (direction === 'up') {
+			item.parentNode.insertBefore(item, sibling);
+		} else {
+			item.parentNode.insertBefore(sibling, item);
+		}
+	}
+
+	document.addEventListener('click', function (event) {
+		var link = event.target.closest('[data-send-to-e-reader-action]');
+		if (!link) {
+			return;
+		}
+
+		event.preventDefault();
+
+		switch (link.getAttribute('data-send-to-e-reader-action')) {
+			case 'select-all':
+				setAll(true);
+				break;
+			case 'select-none':
+				setAll(false);
+				break;
+			case 'reverse-list':
+				reverseList();
+				break;
+			case 'move-up':
+				moveItem(link, 'up');
+				break;
+			case 'move-down':
+				moveItem(link, 'down');
+				break;
+		}
+	});
+}());
--- templates/plain-list.php
+++ templates/plain-list.php
@@ -9,64 +9,18 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 <html><head><title><?php echo esc_html( $args['title'] ); ?></title>
-<script>
-function selectAll() {
-	var checkboxes = document.getElementsByTagName('input');
-	for (var i = 0; i < checkboxes.length; i++) {
-		if (checkboxes[i].type == 'checkbox') {
-			checkboxes[i].checked = true;
-		}
-	}
-	return false;
-}
-function selectNone() {
-	var checkboxes = document.getElementsByTagName('input');
-	for (var i = 0; i < checkboxes.length; i++) {
-		if (checkboxes[i].type == 'checkbox') {
-			checkboxes[i].checked = false;
-		}
-	}
-	return false;
-}
-
-function reverseList() {
-	var list = document.getElementsByTagName('li');
-	var parent = list[0].parentNode;
-	for (var i = 0; i < list.length; i++) {
-		parent.insertBefore(list[i], parent.firstChild);
-	}
-	return false;
-}
-
-function moveUp( element ) {
-	var parent = element.parentNode.parentNode;
-	var prev = parent.previousElementSibling;
-	if ( prev ) {
-		parent.parentNode.insertBefore( parent, prev );
-	}
-	return false;
-}
-
-function moveDown( element ) {
-	var parent = element.parentNode.parentNode;
-	var next = parent.nextElementSibling;
-	if ( next ) {
-		parent.parentNode.insertBefore( next, parent );
-	}
-	return false;
-}
-</script>
+<script src="<?php echo esc_url( plugins_url( 'plain-list.js', dirname( __DIR__ ) . '/send-to-e-reader.php' ) ); ?>" defer></script>
 </head>
 <body>
 	<form>
-		<span style="float: right"><a href="" onclick="return reverseList()">Reverse</a> | <a href="" onclick="return selectAll()">Select all</a> | <a href="" onclick="return selectNone()">Select none</a></span>
+		<span style="float: right"><a href="#" data-send-to-e-reader-action="reverse-list">Reverse</a> | <a href="#" data-send-to-e-reader-action="select-all">Select all</a> | <a href="#" data-send-to-e-reader-action="select-none">Select none</a></span>
 		<button>Download</button>
 
 		<ul>
 		<?php foreach ( $args['posts'] as $post ) : ?>
 				<li><input type="checkbox" name="<?php echo esc_attr( $args['inputname'] ); ?>[]" value="<?php echo esc_attr( $post->ID ); ?>" <?php checked( isset( $args['unsent'][ $post->ID ] ) ); ?> />
 				<?php echo esc_html( get_the_title( $post ) ); ?><br/><small><?php echo esc_html( wp_trim_words( wp_strip_all_tags( $post->post_content ) ) ); ?></small>
-				<small><br><a href="<?php echo esc_html( get_the_permalink( $post ) ); ?>"><?php echo esc_html( get_the_permalink( $post ) ); ?></a> | <a href="" onclick="return moveUp( this )">Move up</a> | <a href="" onclick="return moveDown( this )">Move down</a><br><br></small>
+				<small><br><a href="<?php echo esc_html( get_the_permalink( $post ) ); ?>"><?php echo esc_html( get_the_permalink( $post ) ); ?></a> | <a href="#" data-send-to-e-reader-action="move-up">Move up</a> | <a href="#" data-send-to-e-reader-action="move-down">Move down</a><br><br></small>
 				</li>
 		<?php endforeach; ?>
 		</ul>
